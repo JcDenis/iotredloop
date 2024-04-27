1ca6a199d4557b3bc30a17e9b333674beba8131c
Jean-Christian Paul Denis <github@jcdenis.fr>
2024-04-27 13:24:08 +0200

    Delete src directory

--- src/K_Routine/minute_to_HH_mm.js
+++
@@ -1,7 +0,0 @@
-var d = Number(msg.payload);
-var h = Math.floor(d / 60);
-var m = Math.floor(d % 60);
-if (h < 10) { h = h < 5 ? '00' : '0' + h; }
-if (m < 10) { m = m < 5 ? '00' : '0' + m; }
-msg.payload = h + ':' + m;
-return msg;
